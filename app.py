2ae1daee1f4c933e412254b43d858aab168a1339
stepin aleksey <stepin_a@utkonos.ru>
2020-06-29 23:12:33 +0300

    show file from server

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello Fucking World!'
+    title = 'Hello Fucking World!'
+    # TODO: сделать конфигом
+    # full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'shovon.jpg')
+    full_filename = os.path.join('static', '8.jpg')
+    return render_template('index.html', title=title, h1=full_filename, user_image=full_filename)
+
 
 # @app.route('/')
-@app.route('/index')
-def index():
+@app.route('/test_page')
+def test_page():
     user = {'username': 'BANDIT'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('test_page.html', title='Home', user=user)
 
 
 if __name__ == '__main__':
